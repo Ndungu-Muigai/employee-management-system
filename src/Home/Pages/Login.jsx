b20f5b2a311bdcf13a30d25ab17d3d7eda951e6f
Ndungu Muigai <ndungu.muigai01@gmail.com>
2026-02-28 11:42:12 +0300

    Worked on the responsiveness of the bottom section of the login form

--- src/Home/Pages/Login.jsx
+++ src/Home/Pages/Login.jsx
@@ -111,7 +111,7 @@ const Login = () =>
                         </div>
                     </div>
 
-                    <div className="flex justify-between items-center">
+                    <div className="flex flex-col md:flex-row justify-between items-center">
                         <label className="label">
                             <input type="checkbox" defaultChecked className="checkbox checkbox-accent checkbox-sm" checked={rememberMe} onChange={handleRememberMeChange}/>
                             Remember me
